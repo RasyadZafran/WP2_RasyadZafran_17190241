4a0090bcb8f724ea670dfa36065c09b921162950
unknown <rasyad.firnadi@gmail.com>
2022-04-25 15:35:55 +0700

    first commit

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,9 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>HALLO !!!<?/h1>";
+        echo "Nama saya Rasyad Zafran. Domisili saya di Depok";
+    }
+}
---
+++ application/views/welcome_message.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </title>Latihan Web Programing II</title>
+</head>
+<body> 
+    <p>Nama  : M Rasyad Zafran F.<p>
+    <p>Kelas : 17.6A.01.<p>
+    <p>NIM   : 17190241.<p>
+</body>
+</html>
